a5212cd88ed5012a021510274f36bb8ac9473b86
Cesar Uribe <uribeleon@gmail.com>
2021-08-25 17:54:34 -0500

    fix some troubles in replacement

--- i18n_setlang_sparql.py
+++ i18n_setlang_sparql.py
@@ -19,12 +19,31 @@ def print_info(content, color, fmt):
     ))
     print(Style.RESET_ALL)
 
+def check_and_set_word_order(string,words):
+    string_list = string.split(",")
+    
+    for i,word in enumerate(words):
+        onlyword = re.sub(r'[\{\}\[\] ]','',word)
+        if word in string_list:
+            string_list.remove(word)
+        elif onlyword in string:
+            idx = [ i for i,s in enumerate(string_list) if onlyword in s]
+            string_list = [ elm for i,elm in enumerate(string_list) if i not in idx]
+                
+        string_list.insert(i,word)
+    return ','.join(string_list)
+
+    
+
 def replace_with_sed(filename, old, new, inplace=False):
-    new = new.replace("{{","\{\{").replace("}}","\}\}")
+    # rx = re.compile("([\{\}\[\]])")
+    new = re.escape(new)
+    old = re.escape(old)
     if inplace:
         cmd =[
             "sed",
             "-i",
+            "-r",
             f"s/{old}/{new}/g",
             f"{filename}"
         ]
@@ -42,6 +61,7 @@ def replace_with_sed(filename, old, new, inplace=False):
     else:
         cmd =[
             "sed",
+            "-r",
             f"s/{old}/{new}/g",
             f"{filename}"
         ]
@@ -64,35 +84,44 @@ def cli():
 
 @cli.command()
 @click.argument('filename')
-@click.option('--inplace',is_flag=True)
+@click.option('--inplace',"-i",is_flag=True)
 def findreplace(**kwargs):
     filename = kwargs["filename"]
 
     process = subprocess.run(['cat',filename],stdout=subprocess.PIPE)
     re_obj = re.compile(r'(wikibase:language )("([^"]*)")')
     matches = re_obj.search(process.stdout.decode('utf-8'))
-    languages = matches.group(3)
-    newlang = languages
+    # pdb.set_trace()
+    if matches:
+        languages = matches.group(3)
+        newlang = languages
+        newlang = check_and_set_word_order(languages, [
+            "{{ g.lang_code }}",
+            "[AUTO_LANGUAGE]",
+            "en"
+        ])
+        
 
-    have_langcode = "g.lang_code" in languages
-    have_autolang = "[AUTO_LANGUAGE]" in languages
-
-    if not have_autolang:
-        newlang = f"[AUTO_LANGUAGE],{newlang}"
-
-    if not have_langcode:
-        newlang = f"{{{{ g.lang_code }}}},{newlang}"
-
-    if not (have_langcode or have_autolang):
-        replacement=replace_with_sed(filename, languages, newlang, inplace=kwargs["inplace"])
-        print_info({
-            "Filename": [Path(filename).name], 
-            "Text that will be replaced\n( nline: newtext )": [
-                textwrap.fill(replacement.decode('utf-8'), width=50)
-            ]
-        }, Fore.LIGHTGREEN_EX, "psql")
+        if newlang != languages:
+            
+            if "," not in languages:
+                languages = f'{matches.group(1)}{matches.group(2)}'
+                newlang=f'{matches.group(1)}"{newlang}"'
+            
+            replacement=replace_with_sed(filename, languages, newlang, inplace=kwargs["inplace"])
+            print_info({
+                "Filename": [Path(filename).name], 
+                "Text that will be replaced\n( nline: newtext )": [
+                    textwrap.fill(replacement.decode('utf-8'), width=50)
+                ]
+            }, Fore.LIGHTGREEN_EX, "psql")
+        else:
+            print_info({"INFO: " : ["Nothing to change"]}, Fore.LIGHTRED_EX, "plain")
     else:
-        print_info({"INFO: " : ["Nothing to change"]}, Fore.LIGHTRED_EX, "plain")
+        print_info({
+            "WARNING: ": [f"The file '{Path(filename).name}' not has language option inside"]
+            }, 
+            Fore.LIGHTYELLOW_EX, "plain")
 
 
 
